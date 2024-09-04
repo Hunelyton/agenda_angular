1b1b2520162198616a1febb665d5c33fee0e37aa
Hunelyton <hunelyton@hotmail.com>
2024-09-04 15:41:59 -0300

    ajustes no PosMapping

--- back/src/main/java/com/agenda/contatos/controller/ContatosController.java
+++ back/src/main/java/com/agenda/contatos/controller/ContatosController.java
@@ -3,8 +3,12 @@ package com.agenda.contatos.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.agenda.contatos.model.ContatosModel;
@@ -26,4 +30,10 @@ public class ContatosController {
     public List<ContatosModel> list(){
         return contatosRepository.findAll();
     }
+
+    @PostMapping
+    public ContatosModel create(@RequestBody ContatosModel contatos) {
+        return contatosRepository.save(contatos);
+    }
+
 }
--- back/src/main/java/com/agenda/contatos/repository/ContatosRepository.java
+++ back/src/main/java/com/agenda/contatos/repository/ContatosRepository.java
@@ -7,6 +7,8 @@ import com.agenda.contatos.model.ContatosModel;
 
 @Repository
 public interface ContatosRepository extends JpaRepository<ContatosModel,Long>{
+
+    
     
 
 
